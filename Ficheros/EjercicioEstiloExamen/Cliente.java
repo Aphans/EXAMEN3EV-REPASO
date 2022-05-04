ebc4f3b286474c4fdc5ec4914f090f1fd21182f7
Aphans <rapperanqeel@hotmail.com>
2022-05-04 18:05:05 +0200

    a

--- Ficheros/EjercicioEstiloExamen/Cliente.java
+++ Ficheros/EjercicioEstiloExamen/Cliente.java
@@ -1,9 +1,12 @@
 package EjercicioEstiloExamen;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Cliente {
 	private String dni;
@@ -29,19 +32,74 @@ public class Cliente {
 			linea = br.readLine();
 		}
 	}
-	public void mostrar() {
+	public static void mostrar() {
 		for (String key: productos.keySet()){  
 			System.out.println(key+ " = " + productos.get(key));
 		} 
-
 	}
-	public static void main(String[] args) {
+	public static void añadirArticulo(String producto,int cantidad) {
+		BufferedWriter br = null;
 		try {
-			comprar();
+			 br = new BufferedWriter(new FileWriter("productos.txt",true));
+			br.write(String.valueOf(producto));
+			br.write(" ");
+			br.write(String.valueOf(cantidad));
+			br.newLine();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		finally {
+			if(br!=null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		productos.put(producto, cantidad);
 	}
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean salir = false;
+		while(!salir) {
+			System.out.println("1.Comprar productos"+"\n"+
+					"2.Añadir Productos"+"\n"+
+					"3.Mostrar productos"+"\n"+
+					"4.Eliminar productos"+"\n"+
+					"5.Sacar ticket"+"\n"+
+					"6.Salir");
+			int opcion = sc.nextInt();
+			switch(opcion) {
+			case 1:
+				try {
+					comprar();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			case 2:
+				System.out.println("Escribe el producto que desea añadir");
+				String producto = sc.next();
+				System.out.println("Indique la cantidad del producto a añadir");
+				int cantidad = sc.nextInt();
+				añadirArticulo(producto,cantidad);
+				break;
+			case 3:
+				mostrar();
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				sc.close();
+				salir = true;
+				break;
+			}
+		}
+	}
 }
