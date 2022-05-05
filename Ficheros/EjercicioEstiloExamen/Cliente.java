4904fb3c0f3f06816ba58a180f46f0b1b680e85e
Aphans <54036253+Aphans@users.noreply.github.com>
2022-05-05 10:07:20 +0200

    Creating method sacarTicket();

--- Ficheros/EjercicioEstiloExamen/Cliente.java
+++ Ficheros/EjercicioEstiloExamen/Cliente.java
@@ -2,9 +2,12 @@ package EjercicioEstiloExamen;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -14,22 +17,15 @@ public class Cliente {
 	public static void comprar() throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader("productos.txt"));
 		String linea;
-		linea = br.readLine();
-		while(linea!=null) {
+		while((linea = br.readLine()) !=null) {
 			String datos[]=linea.split(" ");//Parte los datos en posiciones, una a la izquierda del espacio y otra a la derecha
 			String producto = String.valueOf(datos[0]); //Guarda los datos de la izquierda que es un String
-			Integer cantidad = Integer.valueOf(datos[1]);//Guarda los datos de la derecha que es un int
-			Integer cantidadTotal = null;
-			if(!productos.containsKey(producto)) {
-				Integer cantidadRepetida = Integer.valueOf(datos[1]);
-				cantidadTotal= cantidad+cantidadRepetida;
+			Integer cantidad = Integer.valueOf(datos[1]);//Guarda los datos de la derecha que es un int;
+			if(productos.containsKey(producto)) {
+				Integer cantidadRepetida = productos.get(producto);
+				cantidad+= cantidad+cantidadRepetida;
 			}
-			else {
-				Integer cantidadRepetida = Integer.valueOf(datos[1]);
-				cantidadTotal= cantidad+cantidadRepetida;
-			}
-			productos.put(producto, cantidadTotal);
-			linea = br.readLine();
+			productos.put(producto, Integer.valueOf(cantidad));
 		}
 	}
 	public static void mostrar() {
@@ -37,7 +33,7 @@ public class Cliente {
 			System.out.println(key+ " = " + productos.get(key));
 		} 
 	}
-	public static void a�adirArticulo(String producto,int cantidad) {
+	public static void añadirArticulo(String producto,int cantidad) {
 		BufferedWriter br = null;
 		try {
 			 br = new BufferedWriter(new FileWriter("productos.txt",true));
@@ -60,6 +56,65 @@ public class Cliente {
 			}
 		}
 		productos.put(producto, cantidad);
+	}
+	public static void sacarTicket() {
+		File file =  new File("ticket.txt");
+		File file2 = new File("precios.txt");
+		BufferedWriter bw = null;
+		try {
+			bw = new BufferedWriter(new FileWriter(file));
+				for (String key: productos.keySet()){  
+				bw.write(key);
+				bw.write(" ");
+				bw.write(String.valueOf(productos.get(key)));
+				bw.newLine();
+			}
+			
+		} catch (FileNotFoundException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		try {
+			BufferedReader br2 = new BufferedReader(new FileReader(file2));
+			String linea;
+			while((linea=br2.readLine())!=null) {
+				String[] datos=linea.split(" ");
+				double precio = Double.valueOf(datos[1]);
+				int cantidad=0;
+				for (String key : productos.keySet()) {
+					if(key.equals(datos[0])) { //Así 
+						 cantidad = productos.get(key);
+						 System.out.println(cantidad);
+							double precioTotal=+precio;
+							bw.newLine();
+							 DecimalFormat df = new DecimalFormat("#.00");
+							bw.write("Precio Total:"+String.valueOf(df.format(precioTotal*cantidad)));
+					return;
+					}
+				}
+			
+				
+			
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally {
+			if(bw!=null) {
+				try {
+					bw.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
 	}
 	public static void eliminarProducto(String producto,int cantidad) {
 		Integer cantidades = productos.get(producto);
@@ -85,7 +140,7 @@ public class Cliente {
 		boolean salir = false;
 		while(!salir) {
 			System.out.println("1.Comprar productos"+"\n"+
-					"2.A�adir Productos"+"\n"+
+					"2.Añadir Productos"+"\n"+
 					"3.Mostrar productos"+"\n"+
 					"4.Eliminar productos"+"\n"+
 					"5.Sacar ticket"+"\n"+
@@ -101,17 +156,17 @@ public class Cliente {
 				}
 				break;
 			case 2:
-				System.out.println("Escribe el producto que desea a�adir");
+				System.out.println("Escribe el producto que desea añadir");
 				String producto = sc.next();
-				System.out.println("Indique la cantidad del producto a a�adir");
+				System.out.println("Indique la cantidad del producto a añadir");
 				int cantidad = sc.nextInt();
-				a�adirArticulo(producto,cantidad);
+				añadirArticulo(producto,cantidad);
 				break;
 			case 3:
 				mostrar();
 				break;
 			case 4:
-				System.out.println("�Desea eliminar un producto?S/N");
+				System.out.println("¿Desea eliminar un producto?S/N");
 				String eliminar = sc.next();
 				eliminar.toUpperCase();
 				if(eliminar.equals("S")){
@@ -129,6 +184,7 @@ public class Cliente {
 				}
 				break;
 			case 5:
+				sacarTicket();
 				break;
 			case 6:
 				sc.close();
